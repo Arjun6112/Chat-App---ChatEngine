cab6237fc74928671528d90db4bff7161b0dd0bf
Arjun <6112arjun@gmail.com>
2023-08-11 01:26:14 +0530

    Small Change

--- vite-project/src/pages/AuthPage.jsx
+++ vite-project/src/pages/AuthPage.jsx
@@ -18,7 +18,7 @@ const AuthPage = (props) => {
       <form onSubmit={onSubmit} className="form-card">
         <div className="form-title">Welcome 👋</div>
 
-        <div className="form-subtitle">Set a username to get started</div>
+        <div className="form-subtitle">Pick a username to get started</div>
 
         <div className="auth">
           <div className="auth-label">Username</div>
